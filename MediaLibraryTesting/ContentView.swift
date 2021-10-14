7dcf7c1639a1ee75e85cbd7fd5cf723eaa6a6527
Ben Gottlieb <ben@standalone.com>
2021-10-14 05:12:15 -0500

    Initial Commit

---
+++ MediaLibraryTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MediaLibraryTesting
+//
+//  Created by Ben Gottlieb on 10/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediaLibraryTesting/MediaLibraryTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaLibraryTestingApp.swift
+//  MediaLibraryTesting
+//
+//  Created by Ben Gottlieb on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MediaLibraryTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
